b67b6f0ba6f65013c356ec30bf57c23ad53c2db6
tusharsharma045 <tusharsharma02007@gmail.com>
2026-01-21 15:37:50 +0530

    javascript

---
+++ web_dev2.js
@@ -0,0 +1,21 @@
+
+
+// question =  find out the occerence of 'r' in the given string
+
+// let string = "tu meri main tera main teri tu mera";
+// let count = 0;
+
+// for (let i = 0; i < string.length; i++) {
+//     if (string[i] === 'r') {
+//         count++;
+//     }
+// }
+
+// console.log("Occurrence of 'r' =", count);
+
+
+
+
+let string = "tu meri main tera main teri tu mera";
+let res = string.split('r');
+console.log(res);
