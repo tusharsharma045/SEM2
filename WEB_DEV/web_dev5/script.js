3a17bf5d6e520fa770a3feb35b35ff4ec17b1b32
tusharsharma045 <tusharsharma02007@gmail.com>
2026-01-28 15:46:53 +0530

    heading

---
+++ WEB_DEV/web_dev5/script.js
@@ -0,0 +1,16 @@
+const headingElement = document.getElementById('heading');
+console.log('myHeading');
+
+headingElement.textContent = 'Hello from JavaScript!';
+headingElement.style.color = 'blue';
+headingElement.style.fontSize = '24px';
+headingElement.style.fontWeight = 'bold';
+headingElement.style.backgroundColor = 'lightgray';
+headingElement.style.padding = '10px';
+headingElement.style.borderRadius = '5px';
+
+const paragraphElement = document.querySelector('p');
+("#content")
+
+paragraphElement.textContent= 'This paragraph has been modified using querySelector.';
+paragraphElement.style.color = 'green';
