ece49f512c8e8fa6a7cd3b0dc422c1068f5ab094
tusharsharma045 <tusharsharma02007@gmail.com>
2026-02-03 12:36:23 +0530

    task5

--- WEB_DEV/TASK_1/script.js
+++ WEB_DEV/TASK_1/script.js
@@ -1,29 +1,57 @@
+// task 1
+const profile=document.getElementById("profile");
+profile.style.backgroundColor="tomato";
+profile.style.textAlign="center";
+profile.style.padding="15px";
 
-    const userProfile =  Name: "Tushar Sharma",
-        Role: "Developer";
+//  Task 2
+const highlight = document.getElementsByClassName("important");
+console.log(highlight)
+for(let i=0; i<highlight.length; i++){
+    highlight[i].style.color="red"
+    
+}
+  
 
-    const profileSection = document.getElementById("profile");
 
-    profileSection.innerHTML = `
-        <h1>User Profile</h1>
-        <div class="profile-card">
-            <p><strong>Name:</strong> ${userProfile.name}</p>
-            <p><strong>Role:</strong> ${userProfile.role}</p>
-        </div>
+    // task 3
 
-    `;
+    const task3 = document.getElementsByTagName("p");
+    console.log(task3);
+    for(let i=0; i<task3.length; i++){
+        if(i%2===0){
+       task3[i].style.color="blue";
+        }
+        else{
+            task3[i].style.color="green";
+        }
+        }
+        task3[task3.length-1].style.fontWeight="bold";
 
+        // task-4
 
     
-     4. Simple styling
-    profile.style.width = "300px";
-    profile.style.padding = "15px";
-    profile.style.border = "1px solid #ccc";
-    profile.style.margin = "40px auto";
-    profile.style.fontFamily = "Arial";
+    const task4 = document.querySelector(".box");
+    task4.style.backgroundColor="black";
+    task4.style.color="white";
 
-    heading.style.textAlign = "center";
+    // for all the classes or id
+    // const task4 = document.querySelectorAll(".box");
+    // for (let i = 0; i < task4.length; i++) {
+    //     task4[i].style.backgroundColor="black";
+    //     task4[i].style.color="white";
+    //     task4.textContent="in firxt box "    
+    // }
+    // // 
 
-    name.style.fontSize = "14px";
-    role.style.fontSize = "14px";
 
+    // task69-5
+    const task5 = document.querySelectorAll("#content p");
+    for(let i=0; i<task5.length; i++){
+        task5[i].style.color="purple";}
+
+    // const section = document.getElementById("content");
+    // const task5= section.querySelectorAll("p");
+    // for(let i=0; i<task5.length; i++){
+    //     task5[i].style.color="purple";
+    // }
