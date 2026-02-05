641eeaa0b2b1077fe52965847b62773ceae34ece
tusharsharma045 <tusharsharma02007@gmail.com>
2026-02-05 11:56:02 +0530

    first commit

---
+++ WEB_DEV/web_dev7/script.js
@@ -0,0 +1,12 @@
+function handleClick() {
+    document.getElementById("myButton").textContent = "Clicked!";
+}
+
+function handleSecondClick() {
+    document.getElementById("myHeading").style.color = "blue";
+}
+
+function handleThirdClick() {
+    const contentDiv = document.getElementById("content");
+    contentDiv.innerHTML = "<h2>New Heading</h2><img src='image.jpg' alt='Image'>";
+}
