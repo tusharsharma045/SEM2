6f3be5c4af8d6c7e414ebce5df687d63fb782cfc
tusharsharma045 <tusharsharma02007@gmail.com>
2026-02-19 10:11:57 +0530

    first commit

--- DATA_STRUCTURE/dsa_10.py
+++ DATA_STRUCTURE/dsa_10.py
@@ -1,11 +1,22 @@
 # linked list  ==  linked list is a linear data structure which is used to store the data in a non - contigious manner
 
 
-
-def insert_at_beginning(head, data):
-    new_node = Node(data)
-    new_node.next = head
-    head = new_node
-    return head
+class Node:
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
 
 
+class LinkedList:
+    def __init__(self,head = None):
+        self.head = head
+
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def displlay(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end = " ")
+            temp = temp.next
