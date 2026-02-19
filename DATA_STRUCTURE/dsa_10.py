207abf8e4cd88a46905981dccef2533994d753cf
tusharsharma045 <tusharsharma02007@gmail.com>
2026-02-19 10:30:13 +0530

    add a function

--- DATA_STRUCTURE/dsa_10.py
+++ DATA_STRUCTURE/dsa_10.py
@@ -1,5 +1,6 @@
 # linked list  ==  linked list is a linear data structure which is used to store the data in a non - contigious manner
 
+# singly LinkedList ==>>
 
 class Node:
     def __init__(self, data = None, next = None):
@@ -16,13 +17,21 @@ class LinkedList:
         self.head = node
 
 
-    def insert_at_end(self,data):
-        node = Node(data,self.head)
+    def insert_at_end(self, data):
+        node = Node(data)
+        if self.head is None:
+            self.head = node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = node
 
 
 
     def display(self):
         temp = self.head
+
         while temp:
             print(temp.data, end = " ")
             temp = temp.next
@@ -32,4 +41,6 @@ L1 = LinkedList()
 L1.insert_at_beginning(10)
 L1.insert_at_beginning(20)
 L1.insert_at_beginning(100)
-L1.display
+L1.insert_at_end(24)
+L1.insert_at_end(14)
+L1.display()
