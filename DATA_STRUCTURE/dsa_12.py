d4ec38d55788ad1b8c7fcd2de5fc944cbf496c58
tusharsharma045 <tusharsharma02007@gmail.com>
2026-02-23 15:04:28 +0530

    CLASS NODE

--- DATA_STRUCTURE/dsa_12.py
+++ DATA_STRUCTURE/dsa_12.py
@@ -9,13 +9,24 @@ class doubly_linked_list:
     def __init__(self, head = None):
         self.head = head
       
-    def insert_at_end(self, data):
+    def insert_at_beginning(self, data):
         new_node = node(data)
         if not self.head:
             self.head = new_node
         else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = new_node
-            new_node.prev = current
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+    def print_list(self):
+        if self.head is None:
+            print("List is empty")
+            return
+        temp = self.head
+        while temp:
+            print(temp.head, end="  " if temp.next else "\n")
+            temp = temp.next
+
+L1 = doubly_linked_list()
+L1.insert_at_beginning(100)
+L1.insert_at_beginning(200)
+L1.print_list()
