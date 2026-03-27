95ce609fc1ed7a615273291e79566709c02cec07
Tushar Sharma <tusharsharma02007@gmail.com>
2026-03-27 12:33:19 +0530

    ffirst commit

--- DATA_STRUCTURE/dsa_14.py
+++ DATA_STRUCTURE/dsa_14.py
@@ -1,12 +1,22 @@
-class solution:
-    def searchInsert(self, nums:List[int], target:int)->int:
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
         left = 0
         right = len(nums) - 1
         while left <= right:
-            mid = (left + right)//2
-            if nums(mid) == target:
+            mid = (left + right) // 2
+            if nums[mid] == target:
                 return mid
-            elif nums [mid] < target:
-                left = mid+1
+            elif nums[mid] < target:
+                left = mid + 1
             else:
                 right = mid - 1
+        return left
+    
+if __name__ == "__main__":
+    nums = [1, 3, 5, 6]
+    target = 5
+    solver = Solution()
+    print(solver.searchInsert(nums, target))
