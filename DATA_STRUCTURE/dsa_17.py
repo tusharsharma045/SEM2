42064235bff3fb45701c859580625e574d3e6cb1
Tushar Sharma <tusharsharma02007@gmail.com>
2026-04-13 21:32:32 +0530

    first commit

---
+++ DATA_STRUCTURE/dsa_17.py
@@ -0,0 +1,10 @@
+# maximum depth of the binary tree
+class Solution:
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        
+        return 1 + max(left, right)
